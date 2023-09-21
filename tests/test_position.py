e8ebb11f503fb20fcb59b25bd83351e23210bd3e
Julia Smith <juliasmith0517@gmail.com>
2023-09-21 18:04:51 +0000

    Position test 1-0204

--- tests/test_position.py
+++ tests/test_position.py
@@ -4,4 +4,4 @@ from levelup.position import Position
 class TestPosition(TestCase):
     def test_init(self):
         coordinates = Position(0,0)
-        self.assertEqual(coordinates,{"x":0,"y":0})
+        self.assertEqual(coordinates.coordinates,{"x":0,"y":0})
