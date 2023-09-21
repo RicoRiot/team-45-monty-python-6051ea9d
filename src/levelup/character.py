aa9443bf8aa62c920213f1017209b4bba193ed46
Laurie/Zach <laurie.harrison1@gmail.com>
2023-09-21 18:01:18 +0000

    Character get_position test case and implementation

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,9 @@
 class Character:
     name = ""
+    current_position = [-100, -100]
 
     def __init__(self, character_name):
         self.name = character_name
+
+    def get_position(self):
+        return self.current_position
--- tests/test_character.py
+++ tests/test_character.py
@@ -6,3 +6,11 @@ class TestCharacterInitWithName(TestCase):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+    def test_get_position(self):
+        expected_position = {4, 7}
+        testobj = Character("Trogdor")
+        testobj.current_position=expected_position
+        actual_position=testobj.get_position()
+        self.assertEqual(expected_position, actual_position)
+        
