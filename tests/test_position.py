dbaf1d12fe85e18694234d393c0661281896f6a6
Julia Smith <juliasmith0517@gmail.com>
2023-09-21 18:44:26 +0000

    Position test 2

--- tests/test_position.py
+++ tests/test_position.py
@@ -5,3 +5,6 @@ class TestPosition(TestCase):
     def test_init(self):
         coordinates = Position(0,0)
         self.assertEqual(coordinates.coordinates,{"x":0,"y":0})
+
+        coordinates = Position(3,4)
+        self.assertEqual(coordinates.coordinates,{"x":3,"y":4})
