c82e4521dc16fa5d30e6486795c2ef7d3eef33eb
Julia Smith <juliasmith0517@gmail.com>
2023-09-21 18:02:56 +0000

    Position test 1e

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,7 +1,7 @@
 class Position:
-    xPos = 0
-    yPos = 0
-    coordinates = {"x":0,"y":0}
+    xPos = -1
+    yPos = -1
+    coordinates = {"x":-1,"y":-1}
 
     def __init__(self, x, y):
         self.xPos = x
