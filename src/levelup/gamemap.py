2ca167721f112be52866d3230f26628a1419b2a4
Scott <faron.s.richardson@wellsfargo.com>
2023-09-21 20:37:29 +0000

    gamemap calulatePosition testing

--- src/levelup/gamemap.py
+++ src/levelup/gamemap.py
@@ -1,4 +1,5 @@
 from levelup.position import Position
+from levelup.controller import Direction 
 
 class GameMap:
     numPositions = 0
@@ -12,3 +13,22 @@ class GameMap:
 
     def get_positions(self):
         return self.allPositions
+
+    def calculatePosition(self,x, y, Dir):
+        p=Position(x,y)
+        #p = {startingPosition.xPos, startingPosition.yPos}
+        
+        if Dir ==Direction.NORTH:
+            p.yPos=p.yPos+1
+        elif Dir ==Direction.SOUTH:
+            p.yPos=p.yPos-1
+        elif Dir ==Direction.EAST:
+            p.xPos=p.xPos+1
+        elif Dir ==Direction.WEST:
+            p.xPos=p.xPos-1
+        else:
+            pass
+
+        return(p)
+
+        
--- tests/test_game_map.py
+++ tests/test_game_map.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from levelup.gamemap import GameMap
+from levelup.controller import Direction
+from levelup.position import Position
 
 class TestGameMap(TestCase):
     def test_init(self):
@@ -10,3 +12,35 @@ class TestGameMap(TestCase):
     def test_getPositions(self):
         gm = GameMap(100)
         self.assertEqual(list(), gm.get_positions())
+
+    def test_calculatePositionNorth(self):
+        gm = GameMap(100)
+        
+        p2 = gm.calculatePosition(5,5, Direction.NORTH)
+        
+        p=Position(5,6)
+        self.assertEqual(p2.yPos,p.yPos)
+
+    def test_calculatePositionSouth(self):
+        gm = GameMap(100)
+        
+        p2 = gm.calculatePosition(5,5, Direction.SOUTH)
+        
+        p=Position(5,4)
+        self.assertEqual(p2.yPos,p.yPos)
+
+    def test_calculatePositionEast(self):
+        gm = GameMap(100)
+        
+        p2 = gm.calculatePosition(5,5, Direction.EAST)
+        
+        p=Position(6,5)
+        self.assertEqual(p2.yPos,p.yPos)
+
+    def test_calculatePositionWest(self):
+        gm = GameMap(100)
+        
+        p2 = gm.calculatePosition(5,5, Direction.WEST)
+        
+        p=Position(4,5)
+        self.assertEqual(p2.yPos,p.yPos)
